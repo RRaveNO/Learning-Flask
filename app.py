35fbaf2aa1809572c4959257c8132ada2b60b2bb
RRaveNO <neo277@mail.ru>
2024-07-03 16:30:58 +0500

    5 коммит(появились кнопочки)

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def tasks_page():
     return render_template('tasks.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
