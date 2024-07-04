a28b30c0a5d3eb7739b56ba4de06d452ad7a694c
RRaveNO <neo277@mail.ru>
2024-07-04 11:50:39 +0500

    6 коммит(база данных)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,13 +7,40 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/tasks')
-def tasks_page():
-    return render_template('tasks.html')
-
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    return conn
+
+def close_db_connection():
+    conn=get_db_connection()
+    if conn is not None:
+        conn.close()
+
+def init_db():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS checkboxes(id INTEGER PRIMARY KEY, checkbox_column TEXT NOT NULL)''')
+    conn.commit()
+    close_db_connection()
+
+@app.route('/tasks', methods = ['GET', 'POST'])
+def tasks_page():
+    if request.method == 'GET':
+        checkbox_value = request.args.get('checkbox')
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO checkboxes(checkbox_column) VALUES (checkbox_column)", (checkbox_value,))
+        cursor.execute('''SELECT FROM checkboxes''')
+        checkboxes = cursor.fetchall()
+        conn.commit()
+        close_db_connection()
+    return render_template('tasks.html', checkboxes=checkboxes)
+
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
+
